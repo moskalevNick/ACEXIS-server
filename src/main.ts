f40d9011d90f493065f0cb6961afef2b74110d6d
moskalevNick <moskalevnikolay1@gmail.com>
2022-12-01 22:58:25 +0300

    fix too large req

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { urlencoded, json } from 'express';
 import { AppModule } from './app.module';
 import { initializeFirebaseApp } from './config/firebase.config';
 import { corsOptions } from './etc/corsOptions';
@@ -9,6 +10,8 @@ initializeFirebaseApp();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(corsOptions);
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
 
   const config = new DocumentBuilder()
     .setTitle('Exis API')
