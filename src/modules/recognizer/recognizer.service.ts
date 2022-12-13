3e4a370c058bc56703e8046f35b9c0c0f99b81d6
moskalevNick <moskalevnikolay1@gmail.com>
2022-12-13 19:06:24 +0300

    fix

--- src/modules/recognizer/recognizer.service.ts
+++ src/modules/recognizer/recognizer.service.ts
@@ -40,8 +40,6 @@ export class RecognizerService {
   }
 
   async check(checkClientDto: any): Promise<any> {
-    console.log('req: ', checkClientDto);
-
     if (checkClientDto.mode === 'face_event') {
       if (checkClientDto.faces.length) {
         await checkClientDto.faces.forEach(async (face: any) => {
