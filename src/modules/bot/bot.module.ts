08946e0322c955047b762dcd09060a27626f9fe2
moskalevNick <moskalevnikolay1@gmail.com>
2022-12-20 11:20:10 +0300

    fix bot

--- src/modules/bot/bot.module.ts
+++ src/modules/bot/bot.module.ts
@@ -1,14 +1,15 @@
+import { RecognizerModule } from './../recognizer/recognizer.module';
 import { Module } from '@nestjs/common';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { BotUpdate } from './bot.update';
-require('dotenv').config();
 
 @Module({
   imports: [
     TelegrafModule.forRoot({
       token: process.env.BOT_TOKEN,
     }),
+    RecognizerModule,
   ],
-  providers: [BotUpdate],
+  providers: [BotUpdate, RecognizerModule],
 })
 export class BotModule {}
